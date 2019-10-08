e9bd0eebdc201b5575d0540184e74744d6a223c5
Travis Nichols <travisarthurnichols@gmail.com>
2019-10-08 10:53:20 -0500

    Update HUFFMANCLIENT.m

--- HUFFMANCLIENT.m
+++ HUFFMANCLIENT.m
@@ -4,11 +4,12 @@ sample = "AAABBCCDEF";
 dictionary = HUFFMANCLASS.dictionary(sample)
 encodedSample = HUFFMANCLASS.encoder(dictionary.input,dictionary)
 decodedSample = HUFFMANCLASS.decoder(encodedSample,dictionary)
-
+math = HUFFMANCLASS.entropyCalculation(dictionary)
 
 % clc
 % clear
-% sample = "AAABBCCDEF";
+% sample = "abbaccad";
 % dictionary = HUFFMANCLASSTEST.dictionary(sample)
 % encodedSample = HUFFMANCLASSTEST.encoder(dictionary.input,dictionary)
 % decodedSample = HUFFMANCLASSTEST.decoder(encodedSample,dictionary)
+% math = HUFFMANCLASSTEST.entropyCalculation(dictionary)
